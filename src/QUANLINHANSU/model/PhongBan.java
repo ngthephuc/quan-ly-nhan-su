225eef28a879387f8bdd8ca4fa0b0c01b2ff34fa
khải  phạm <29khaipham@gmail.com>
2026-03-09 09:54:36 +0700

    Add files via upload
    update

---
+++ src/QUANLINHANSU/model/PhongBan.java
@@ -0,0 +1,39 @@
+package QUANLINHANSU.model;
+
+public class PhongBan {
+    private String maPb;
+    private String tenPb;
+    private Double heSoLuong;
+
+
+    public PhongBan(String maPb, String tenPb, Double heSoLuong) {
+        this.maPb = maPb;
+        this.tenPb = tenPb;
+        this.heSoLuong = heSoLuong;
+    }
+
+    public String getMaPb() {
+        return maPb;
+    }
+
+    public String getTenPb() {
+        return tenPb;
+    }
+
+    public Double getHeSoLuong() {
+        return heSoLuong;
+
+    }
+
+    public void setMaPb(String maPb) {
+        this.maPb = maPb;
+    }
+
+    public void setTenPb(String tenPb) {
+        this.tenPb = tenPb;
+    }
+
+    public void setHeSoLuong(Double heSoLuong) {
+        this.heSoLuong = heSoLuong;
+    }
+}
