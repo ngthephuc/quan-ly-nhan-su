28f7a0e29f62367f0ba13330b7c042e27e466da7
tuan <goku20x06@gmail.com>
2026-04-05 15:04:46 +0700

    test

--- src/QUANLINHANSU/main/Main.java
+++ src/QUANLINHANSU/main/Main.java
@@ -1,5 +1,6 @@
 package QUANLINHANSU.main;
 
+import QUANLINHANSU.util.SessionManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,22 +10,38 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/fxml/MainView.fxml")
-        );
+        FXMLLoader loader;
+
+        if (SessionManager.isLoggedIn()) {
+            loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+        } else {
+            loader = new FXMLLoader(getClass().getResource("/fxml/scene_login.fxml"));
+        }
 
         Scene scene = new Scene(loader.load());
-
-        stage.setTitle("Test UIk");
         stage.setScene(scene);
+        stage.setTitle("Quản Lý Nhân Sự");
+        stage.setResizable(false);
         stage.show();
+
+
+
+
+
+
+
+
+//        FXMLLoader loader = new FXMLLoader(
+//                getClass().getResource("/fxml/MainView.fxml")
+//        );
+//
+//        Scene scene = new Scene(loader.load());
+//
+//        stage.setTitle("Quản Lý Nhân Sự");
+//        stage.setScene(scene);
+//        stage.show();
     }
     public static void main(String[] args) {
-
-//        EntityManagerFactory emf =
-//                Persistence.createEntityManagerFactory("QuanLyNhanSuPU");
-//        System.out.println("Ket noi thanh cong!");
-
         launch();
     }
 }
